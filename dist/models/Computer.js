b10e24c012e29196532d0b345696972107301deb
KiritoEM <johankirito64@gmail.com>
2024-09-15 19:48:53 +0300

    features: add minimax and prunning

--- dist/models/Computer.js
+++ dist/models/Computer.js
@@ -1,5 +1,58 @@
+import { PlayerType } from "../utils/types.js";
 export class Computer {
-    constructor() { }
-    computerMove() { }
-    minimaxAlphaBeta(depth, alpha, beta, isMaximizing) { }
+    constructor() {
+        this.AIPlayer = PlayerType.player1;
+        this.player = PlayerType.player2;
+        this.alpha = -Infinity;
+        this.beta = Infinity;
+    }
+    computerMove(board) {
+        let boarderState = board.getBoard();
+    }
+    //focntion minimax récursive
+    minimax(depth, alpha, beta, isMaximizing, board, turn) {
+        let boarderState = board.getBoard();
+        let winner = board.checkWinner(turn);
+        //condition d' arrêt de la fonction récursive
+        if (winner === 1)
+            return 1; // l' IA gagne
+        if (winner === 2)
+            return -1; // le joueur gagne
+        if (isMaximizing) {
+            let maxScore = Infinity;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (boarderState[i][j] === 0) {
+                        boarderState[i][j] = this.AIPlayer;
+                        let score = this.minimax(depth - 1, alpha, beta, false, board, turn);
+                        boarderState[i][j] = 0;
+                        maxScore = Math.max(maxScore, score);
+                        alpha = Math.max(alpha, score);
+                        if (beta <= alpha) {
+                            break;
+                        }
+                    }
+                }
+            }
+            return maxScore;
+        }
+        else {
+            let minScore = Infinity;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (boarderState[i][j] === 0) {
+                        boarderState[i][j] = this.player;
+                        let score = this.minimax(depth + 1, alpha, beta, true, board, turn);
+                        boarderState[i][j] = 0;
+                        minScore = Math.min(minScore, score);
+                        beta = Math.max(beta, score);
+                        if (beta <= alpha) {
+                            break;
+                        }
+                    }
+                }
+            }
+            return minScore;
+        }
+    }
 }
--- src/models/Computer.ts
+++ src/models/Computer.ts
@@ -1,7 +1,82 @@
+import { PlayerType } from "../utils/types.js";
+import { Board } from "./Board.js";
+
 export class Computer {
   constructor() {}
+  private AIPlayer: PlayerType = PlayerType.player1;
+  private player: PlayerType = PlayerType.player2;
+  private alpha: number = -Infinity;
+  private beta: number = Infinity;
 
-  public computerMove() {}
+  public computerMove(board: Board) {
+    let boarderState = board.getBoard();
+  }
 
-  public minimaxAlphaBeta(depth, alpha, beta, isMaximizing) {}
+  //focntion minimax récursive
+  public minimax(
+    depth: number,
+    alpha: number,
+    beta: number,
+    isMaximizing: boolean,
+    board: Board,
+    turn: PlayerType
+  ) {
+    let boarderState = board.getBoard();
+    let winner = board.checkWinner(turn);
+
+    //condition d' arrêt de la fonction récursive
+    if (winner === 1) return 1; // l' IA gagne
+    if (winner === 2) return -1; // le joueur gagne
+
+    if (isMaximizing) {
+      let maxScore = Infinity;
+
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (boarderState[i][j] === 0) {
+            boarderState[i][j] = this.AIPlayer;
+            let score = this.minimax(
+              depth - 1,
+              alpha,
+              beta,
+              false,
+              board,
+              turn
+            );
+            boarderState[i][j] = 0;
+
+            maxScore = Math.max(maxScore, score!);
+            alpha = Math.max(alpha, score!);
+
+            if (beta <= alpha) {
+              break;
+            }
+          }
+        }
+      }
+
+      return maxScore;
+    } else {
+      let minScore = Infinity;
+
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (boarderState[i][j] === 0) {
+            boarderState[i][j] = this.player;
+            let score = this.minimax(depth + 1, alpha, beta, true, board, turn);
+            boarderState[i][j] = 0;
+
+            minScore = Math.min(minScore, score!);
+            beta = Math.max(beta, score!);
+
+            if (beta <= alpha) {
+              break;
+            }
+          }
+        }
+      }
+
+      return minScore;
+    }
+  }
 }
